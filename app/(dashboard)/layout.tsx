706a1edc3e5cd32878d36db85f685667b7703c05
thenishantgiri <nishantkrgiri@gmail.com>
2023-11-01 15:50:02 +0530

    added NewTodoForm component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,12 @@
+import NewTodoForm from '@/components/NewTodoForm';
+
 const DashboardLayout = ({ children }: any) => {
   return (
     <div>
-      <h1>Dashboard Layout</h1>
+      <h1>Dashboard</h1>
+      <div>
+        <NewTodoForm />
+      </div>
       {children}
     </div>
   );
