ad854c2a0d3354a21545772993233a5efb5bed98
thenishantgiri <nishantkrgiri@gmail.com>
2023-11-02 18:25:41 +0530

    added support for completing a todo

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,5 +1,21 @@
+'use client';
+
+import { completeTodo } from '@/utils/actions';
+import { useTransition } from 'react';
+
 const Todo = ({ todo }: any) => {
-  return <div>{todo.content}</div>;
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <div
+      className={`border cursor-pointer border-black/10 m-2 pl-6 pr-4 ${
+        todo.completed ? 'line-through text-gray-600' : ''
+      }`}
+      onClick={() => startTransition(() => completeTodo(todo.id))}
+    >
+      {todo.content}
+    </div>
+  );
 };
 
 export default Todo;
