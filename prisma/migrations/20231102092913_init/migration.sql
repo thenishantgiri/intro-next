b1941e94bb01f745d372321bc9879308e7611d53
thenishantgiri <nishantkrgiri@gmail.com>
2023-11-02 18:18:37 +0530

    schema change

--- prisma/migrations/20231102092913_init/migration.sql
+++ prisma/migrations/20231102092913_init/migration.sql
@@ -3,5 +3,5 @@ CREATE TABLE "Todo" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "content" TEXT NOT NULL,
-    "completedAt" BOOLEAN NOT NULL DEFAULT false
+    "completed" BOOLEAN NOT NULL DEFAULT false
 );
