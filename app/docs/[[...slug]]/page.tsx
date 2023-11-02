903a7a3b34b406302bee85d2e26c43e2ac5d619b
thenishantgiri <nishantkrgiri@gmail.com>
2023-11-02 18:23:42 +0530

    docs route slug

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -1,6 +1,26 @@
-const DocsSlugPage = ({ params }: any) => {
-  // console.log(params.slug);
-  return <div>Slug: {params.slug[1]}</div>;
+import content from '@/utils/content.json';
+
+export const generateStaticParams = () => {
+  return Object.keys(content).map((slug) => ({
+    slug: slug.split('/'),
+  }));
 };
 
-export default DocsSlugPage;
+const getData = (slug: any) => {
+  const path = slug.join('/');
+  return content[path] || 'coming soon';
+};
+
+const DocsPage = ({ params }) => {
+  const data = getData(params.slug || []);
+  return (
+    <div>
+      <h1 className="text-3xl">Docs Page</h1>
+      <div>
+        <p className="text-xl">{data}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DocsPage;
