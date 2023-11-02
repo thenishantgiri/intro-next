381903d8fa89e349af50be88b153cf5cba8cabf2
thenishantgiri <nishantkrgiri@gmail.com>
2023-11-02 18:24:38 +0530

    docs layout

---
+++ app/docs/layout.tsx
@@ -0,0 +1,12 @@
+const DocsLayout = ({ children }: any) => {
+  return (
+    <div>
+      <div>
+        <h1>Docs</h1>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default DocsLayout;
